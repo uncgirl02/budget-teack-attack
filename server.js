b38236dd9caafe2eb8e3380a8b67cda3315f972f
Erin Ewart <ems1980@hotmail.com>
2022-06-13 23:01:59 -0400

    deploying to heroku

--- server.js
+++ server.js
@@ -16,9 +16,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:3001/', {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
